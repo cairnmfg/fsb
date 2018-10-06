ec4fa18aa21857dc1ec10cf89c5481d3559a7775
Dylan Fareed <email@dylanfareed.com>
2018-10-06 14:20:47 -0400

    device tree files for field devices

---
+++ field/README.md
@@ -0,0 +1,1 @@
+This directory contains custom device tree files for Field compute module carrier boards.
--- post-createfs.sh
+++ post-createfs.sh
@@ -6,3 +6,6 @@ FWUP_CONFIG=$NERVES_DEFCONFIG_DIR/fwup.conf
 
 # Run the common post-image processing for nerves
 $BR2_EXTERNAL_NERVES_PATH/board/nerves-common/post-createfs.sh $TARGET_DIR $FWUP_CONFIG
+
+# Copy the Field device tree files to the images dir
+cp -rf $NERVES_DEFCONFIG_DIR/field $BINARIES_DIR
